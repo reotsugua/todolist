556bc35dee1d9224cca94eb143b50edd1aff0b20
reotsugua <renanaugusto.ras@gmail.com>
2024-12-25 16:35:53 -0300

    fazendo

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+import '../css/list-groups.css'
+import './color-modes.js'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
