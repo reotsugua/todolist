91aaee93e087acc5ef65b2f7099724da9e4eb91c
Renan Santos <renan.santos@grupoeuro17.com.br>
2025-01-10 23:09:03 -0300

    testando visuais

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default {
         name: 'Todolist por Renan Augusto',
         short_name: 'Todolist',
         description: 'Descrição do Todolist',
-        theme_color: '#3367D6',
+        // theme_color: '#3367D6',
         icons: [{
             src: 'android-chrome-192x192.png',
             sizes: '192x192',
